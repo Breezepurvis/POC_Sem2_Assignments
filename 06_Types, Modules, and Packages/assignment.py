ee0e706f03b8ac008f1a4b088c3e2f803b203df1
Breeze Purvis <purvibre000@warren.k12.in.us>
2024-02-29 11:52:53 -0500

    no

--- 06_Types, Modules, and Packages/assignment.py
+++
@@ -1,6 +0,0 @@
-def average(a, b) float
-         return (a +b) / 2
-
-number1: float= float(input("Enter a decimal number:"))
-number2: float= float(input("Enter another decimal number:"))
-print(average(number1, number2)
